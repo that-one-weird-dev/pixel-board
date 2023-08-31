55115989a7f6d4dc31afc1e682e763c239f04705
N3rvy <ugoccionimanuel@gmail.com>
2023-08-31 17:41:57 +0200

    Undoable clear action

--- app/src/main/java/com/zeropixel/pixelboard/canvas/actions/ClearAction.kt
+++ app/src/main/java/com/zeropixel/pixelboard/canvas/actions/ClearAction.kt
@@ -4,10 +4,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.zeropixel.pixelboard.canvas.undo.UndoablePixelsBuilder
 import com.zeropixel.pixelboard.canvas.utils.rectanglePixels
 import com.zeropixel.pixelboard.views.CanvasViewModel
 
 class ClearAction : Action {
+
+    private val undoableBuilder = UndoablePixelsBuilder()
 
     @Composable
     override fun icon(): ImageVector {
@@ -16,7 +19,11 @@ class ClearAction : Action {
 
     override fun CanvasViewModel.execute() {
         rectanglePixels(0, 0, canvas.width, canvas.height) { x, y ->
-            canvas.setPixel(currentLayerId, x, y, 0x00000000)
+            val oldPixel = canvas.setPixel(currentLayerId, x, y, 0x00000000)
+
+            undoableBuilder.addPixel(x, y, oldPixel)
         }
+
+        pushUndoable(undoableBuilder.build("Clear screen", currentLayerId))
     }
 }
