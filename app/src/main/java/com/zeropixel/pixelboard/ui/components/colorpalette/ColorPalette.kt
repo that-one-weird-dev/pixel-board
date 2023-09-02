01b736ba29b3d58494bc15e53a6b9ea28154754e
manu <manuelugoccioni@gmail.com>
2023-09-02 02:20:05 +0200

    Change color palette grid ui

--- app/src/main/java/com/zeropixel/pixelboard/ui/components/colorpalette/ColorPalette.kt
+++ app/src/main/java/com/zeropixel/pixelboard/ui/components/colorpalette/ColorPalette.kt
@@ -5,14 +5,15 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.zeropixel.pixelboard.canvas.palette.ColorPalette
@@ -27,24 +28,32 @@ fun ColorPalette(
 
     columns: Int,
 ) {
-    LazyVerticalGrid(
+    Column(
         modifier = modifier,
-
-        columns = GridCells.Fixed(columns),
-
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
-        itemsIndexed(palette.colors) { i, color ->
-            val isSelected = color == currentColor
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(columns),
+        ) {
+            itemsIndexed(palette.colors) { i, color ->
+                val isSelected = color == currentColor
 
-            Color(
-                Modifier,
-                isSelected,
-                color,
-                onClick = { onColorPick(i) }
-            )
+                Color(
+                    Modifier,
+                    isSelected,
+                    color,
+                    onClick = { onColorPick(i) }
+                )
+            }
         }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .background(Color(currentColor))
+                .border(2.dp, Color.Black),
+        )
     }
 }
 
@@ -55,16 +64,14 @@ private fun Color(
     color: ColorInt,
     onClick: () -> Unit,
 ) {
-    val scale = if (isSelected) .9f else 1f
+    val border = if (isSelected) 4.dp else 1.dp
 
     Box(
         modifier = Modifier
             .size(40.dp)
-            .scale(scale)
             .then(modifier)
-            .aspectRatio(1f)
             .background(Color(color))
             .clickable(onClick = onClick)
-            .border(1.dp, Color.Black)
+            .border(border, Color.Black)
     )
 }
